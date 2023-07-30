3991759b72e5d7b96eefc7363ae37c155fedaf81
Юлиана Стрельникова <julianka.str@gmail.com>
2023-07-30 09:13:39 +0300

    add permission

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -26,3 +26,11 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return (obj.author == request.user or request.method
                 in permissions.SAFE_METHODS)
 
+class IsAuthorOnly(permissions.BasePermission):
+    """Доступ разрешен только автору."""
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (obj.author == request.user)
