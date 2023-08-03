b32a34be4c42c58a4a73265b74c8f79f66207a87
Юлиана Стрельникова <julianka.str@gmail.com>
2023-08-03 09:23:40 +0300

    add read_file

--- backend/recipes/management/commands/read_files.py
+++ backend/recipes/management/commands/read_files.py
@@ -1,27 +1,18 @@
 import csv
 import os
 
-from django.conf import settings
+from foodgram_backend import settings
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
 
 
-
 def read_ingredients():
-    with open(
-            os.path.join(
-            settings.BASE_DIR,
-            'data', 'ingredients.csv',
-            ),
-            'r', encoding='utf-8'
-    ) as f:
+    path = os.path.join('foodgram-project-react', 'data', 'ingredients.csv')
+    with open(path, 'r', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
-            if row[0] == 'id':
-                continue
             Ingredient.objects.get_or_create(
-                id=row[0],
                 name=row[1],
                 measurement_unit=row[2],
             )
