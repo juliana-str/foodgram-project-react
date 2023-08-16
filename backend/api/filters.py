4b71b8b205d0afbef5a56522ce40a481d048bb62
Юлиана Стрельникова <julianka.str@gmail.com>
2023-08-16 12:43:20 +0300

    add filters

---
+++ backend/api/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters
+from recipes.models import Recipe, Tag
+
+
+class RecipeFilter(filters.FilterSet):
+    """Фильтры рецептов."""
+    author = filters.CharFilter(field_name='recipe__author')
+    tags = filters.MultipleChoiceFilter(field_name='tags__slug')
+    is_favorited = filters.BooleanFilter()
+    is_in_shopping_cart = filters.BooleanFilter()
+
+    class Meta:
+        model = Recipe
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
