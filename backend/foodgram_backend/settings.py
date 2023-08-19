162ae18bd572c7086a9c90cd4e272cac4d80f800
Юлиана Стрельникова <julianka.str@gmail.com>
2023-08-19 21:26:59 +0300

    fix pep8

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -74,13 +74,13 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
-                'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-                'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-                'HOST': os.getenv('DB_HOST', 'localhost'),
-                'PORT': os.getenv('DB_PORT', 5432)
-            }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+            'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', 5432)
+        }
 
     }
 
