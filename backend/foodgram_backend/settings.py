acab272d38d8eb1901ca4d73cdfd46f72f99e542
Юлиана Стрельникова <julianka.str@gmail.com>
2023-08-21 08:55:29 +0300

    Add Actions

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -115,7 +115,7 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
