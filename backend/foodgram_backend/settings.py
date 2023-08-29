f7235f17153627ff5e323d1132b44cf0c216ef8c
Юлиана Стрельникова <julianka.str@gmail.com>
2023-08-29 12:51:31 +0300

    fix env(im)

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,16 +1,17 @@
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '1234')
 
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = True
-ALLOWED_HOSTS=['158.160.30.4', 'foodgramproject.myddns.me', '127.0.0.1', 'localhost']
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
