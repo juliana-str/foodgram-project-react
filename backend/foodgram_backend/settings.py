c27da0424ae2f092885e998db6d542e4024e0385
Юлиана Стрельникова <julianka.str@gmail.com>
2023-09-06 08:53:19 +0300

    allowed hosts

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '1234')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
